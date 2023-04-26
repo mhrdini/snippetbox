9e96d8068cf49790d9ffc37ae9e33a42f3a8ece8
Dini Putri <mahardini.rizky@gmail.com>
2023-04-26 15:41:18 +1000

    (2.5) Customised HTTP headers

--- main.go
+++ main.go
@@ -8,16 +8,30 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a showSnippet handler Junction.
+// Add a showSnippet handler function.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
-// Add a createSnippet handler Junction.
+// Add a createSnippet handler function.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
