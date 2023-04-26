ceb8def3b367561f22f6ce0f5bacac2323faa1ce
Dini Putri <mahardini.rizky@gmail.com>
2023-04-27 00:53:49 +1000

    (2.9) Serving and using static files in templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,14 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Use the relative path to create a file server at that path
+	fs := http.FileServer(http.Dir("../../ui/static/"))
+	// When this handler receives a request, it will remove the leading slash from the URL path and
+	// then search the file server directory for the corresponding file to send the user.
+	// This is done by stripping the leading "/static" from the URL path before passing it to the file
+	// server, otherwise it will be looking for a file which does not exist
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
+
 	// http.ListenAndServe starts a new web server
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
