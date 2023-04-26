5f67174fdc57f94cc68204e566c7edd5ba9c2207
Dini Putri <mahardini.rizky@gmail.com>
2023-04-26 15:48:06 +1000

    (2.6) Added URL query string retrieval

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -16,12 +18,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a showSnippet handler function.
+// Add a showSnippet handler function that receives an id query parameter
+// that must be an integer greater than or equal to 1.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-// Add a createSnippet handler function.
+// Add a createSnippet handler function that only receives POST requests.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "POST" {
