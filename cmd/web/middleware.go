a26c5690f11f9de467e5fee6abd5f72ac03c54a4
Dini Putri <mahardini.rizky@gmail.com>
2024-05-24 20:22:24 +1000

    Middleware, panic recovery

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,12 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // Middleware can be executed:
 // - before servemux -> applied to all requests
 // - after servemux -> applied to some requests
 
 // This is set either in routes or main
+
+// /* Middleware template: */
+// func myMiddleware(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		/* any code here will execute on the way down the chain */
+//    /* any early returns here will revert control back upstream, this pattern is used to restrict access (e.g. authorization) */
+// 		next.ServeHTTP(w, r)
+// 		/* any code here will execute on the way up the chain */
+// 	})
+// }
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -26,12 +39,21 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	})
 }
 
-// /* Middleware template: */
-// func myMiddleware(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		/* any code here will execute on the way down the chain */
-//    /* any early returns here will revert control back upstream, this pattern is used to restrict access (e.g. authorization) */
-// 		next.ServeHTTP(w, r)
-// 		/* any code here will execute on the way up the chain */
-// 	})
-// }
+// Use recover() to check if a panic happened and defer to call serverError when it has panicked
+// NOTE: Will only recover panics happening in the same goroutine that executed this middleware
+// Need to use the following pattern to recover from panics within a defer func within a goroutine
+// func: if err := recover(); err != nil { log.Print(fmt.Errorf("%s\n%s", err, debug.Stack())) }
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				// Setting this acts as a trigger to make Go's HTTP/1 server auto-close the connection
+				w.Header().Set("Connection", "close")
+				// Normalise any-typed error from recover() into an Errorf object format
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,5 +23,5 @@ func (app *application) routes(cfg *Config) http.Handler {
 	mux.HandleFunc("/snippet", app.viewSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 
-	return app.logRequest(secureHeaders(mux))
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
